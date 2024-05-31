384f34daa2b306854c458e863ab5d3501a7831d4
wim.drapier <wim.drapier@gmail.com>
2024-05-31 08:22:12 +0000

    Created restclient app.

---
+++ restclient-app/src/main/java/dev/wdrap/restclient_app/RestclientAppApplication.java
@@ -0,0 +1,13 @@
+package dev.wdrap.restclient_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestclientAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestclientAppApplication.class, args);
+	}
+
+}
---
+++ restclient-app/src/test/java/dev/wdrap/restclient_app/RestclientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wdrap.restclient_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestclientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
